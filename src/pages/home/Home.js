89cc68444301863c7052d72fa83fd5082a472d21
TranManhKhuong <tranmanhkhuong1586@gmail.com>
2024-02-21 15:16:47 +0700

    update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -15,86 +15,88 @@ import { faThemeisle } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
   return (
-    <div className={classes.home}>
-      <div className={classes.home__title}>
-        <div className={classes.home__title__newchat}>
-          <Newchat />
-        </div>
-        <div className={classes.home__title__chatbox}>
-          <div className={classes.wrap}>
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-          </div>
-        </div>
-      </div>
-      <div className={classes.home__content}>
-        <div className={classes.home__content__nav}>
-          <h5>ChatBot</h5>
-          <div className={classes.icon}>
-            <FontAwesomeIcon icon={faThemeisle} color='white' size='xl' />
-          </div>
-        </div>
-        <div className={classes.home__content__wrap}>
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-        </div>
-        <div className={classes.home__content__input}>
-          <Inputtext />
-        </div>
-      </div>
-    </div>
-    // <div>
-    //   <h1>ChatGPT React App</h1>
-    //   <Chat />
+    // <div className={classes.home}>
+    //   <div className={classes.home__title}>
+    //     <div className={classes.home__title__newchat}>
+    //       <Newchat />
+    //     </div>
+    //     <div className={classes.home__title__chatbox}>
+    //       <div className={classes.wrap}>
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //         <Chatbox />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className={classes.home__content}>
+    //     <div className={classes.home__content__nav}>
+    //       <h5>ChatBot</h5>
+    //       <div className={classes.icon}>
+    //         <FontAwesomeIcon icon={faThemeisle} color='white' size='xl' />
+    //       </div>
+    //     </div>
+    //     <div className={classes.home__content__wrap}>
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //       <Content />
+    //     </div>
+    //     <div className={classes.home__content__input}>
+    //       <Inputtext />
+    //     </div>
+    //   </div>
     // </div>
+
+    // đừng xóa comment ở trên nha
+    <div>
+      <h1>ChatGPT React App</h1>
+      <Chat />
+    </div >
   );
 }
 
